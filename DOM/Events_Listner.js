8ab36642cb0f8d69e84114d0d965df92ca792ad1
dristi199 <dristineupane760@gmail.com>
2025-10-31 23:45:44 +0545

    Update

---
+++ DOM/Events_Listner.js
@@ -0,0 +1,6 @@
+//Events:
+// Just announcement by browser  activities like cancelIdleCallback,doubleclick,scroll,keypair
+k
+//Event-target:elements present in HTML code are event target 
+
+//Syntax:<event-target>addEventListner(<event-type>,<function-action>)
